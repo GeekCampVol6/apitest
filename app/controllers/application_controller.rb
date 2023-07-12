210f0df5761fd00b861b6c3d0b8228717d3d506e
emiminate <matsu130830@gmail.com>
2023-07-12 23:34:54 +0900

    進行中?

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,11 @@ require 'net/http'
 class ApplicationController < ActionController::Base
     def show
         uri = URI.parse('https://connpass.com/api/v1/event/')
-        uri.query = URI.encode_www_form({event_id:params[:event_id]})
+        # uri.query = URI.encode_www_form({event_id:params[:event_id]})
+        query_params = {}
+        query_params[:event_id] = params[:event_id] if params[:event_id].present?
+        query_params[:keyword] = params[:keyword] if params[:keyword].present?
+        uri.query = URI.encode_www_form(query_params)
         json = Net::HTTP.get(uri)
         @events = JSON.parse(json)
 
