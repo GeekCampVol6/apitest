1c8b6e628ab98779af77cfde9be63a04eb61fadf
emiminate <matsu130830@gmail.com>
2023-07-12 23:52:18 +0900

    作成中

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
+メモ用
 api使ったことないので色々試し中
 connpassのapiを今使ってなんかしてます
+
+connpassapiのリファレンスサイト
+https://connpass.com/about/api/
+
+query_params[:event_id] = params[:event_id] if param[:event_id].present?
+はこれだったらevent_idというパラメータがあったらurlの後ろについてた時に表示されるみたいな?これ無くて,区切りでやると両方無いと表示されないと思う。
+
+今直接打ち込んで表示してる(JSONデータ見れるだけ)から、フロント側で送ってきたデータをどういう風に反映させるのかが分からないので調べ中...
+↑これrender plain: @events.to_sがプレーンテキスト(文字とかフォーマットがないやつ)で表示してる(htmlがない)からJSONだけ見えてるっぽい？html用意してなんかしてみる
+        
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,17 @@ class ApplicationController < ActionController::Base
     def show
         uri = URI.parse('https://connpass.com/api/v1/event/')
         # uri.query = URI.encode_www_form({event_id:params[:event_id]})
+        # query_paramsの中にif文で入ってきたやつをが入ってる?
         query_params = {}
         query_params[:event_id] = params[:event_id] if params[:event_id].present?
         query_params[:keyword] = params[:keyword] if params[:keyword].present?
+        query_params[:ymd] = params[:ymd] if params[:ymd].present?
+        #ここでJSON方式にエンコードしてるっぽい
         uri.query = URI.encode_www_form(query_params)
         json = Net::HTTP.get(uri)
+        #JSONの文字列を解析してruby用の配列とかに置きなおしている
         @events = JSON.parse(json)
-
+        #プレーンテキストで表示(htmlを作ってないから)
         render plain: @events.to_s
     end
 end
