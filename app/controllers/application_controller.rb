41b4b51c9d61c051dc889257c6bb9cb43759a8a6
emiminate <matsu130830@gmail.com>
2023-07-12 23:12:52 +0900

    api作成中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
+require 'net/http'
+
 class ApplicationController < ActionController::Base
+    def show
+        uri = URI.parse('https://connpass.com/api/v1/event/')
+        uri.query = URI.encode_www_form({event_id:params[:event_id]})
+        json = Net::HTTP.get(uri)
+        @events = JSON.parse(json)
+
+        render plain: @events.to_s
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get '/show', to: 'application#show'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
